e63894f9a08b6a74ad00ed9304c32ae0c4d6f0de
unknown <ghaliarose89@gmail.com>
2021-08-29 12:41:19 -0500

    creating var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Java Scrip is running!");
+var playername= window.prompt ("what's your robot's name?");
+function fight() { window.alert("The fight has begus!");}
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our player name is "+ playername);
+
+
+
+
+//fight();
+
